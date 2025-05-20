fix softmax direction, but leave it for now since similar to implementation online

assignment3/cs231n/transformer_layers.py
@@ -166,7 +166,8 @@ class MultiHeadAttention(nn.Module):
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
         # l->S, d->E from notebook to .py, T = S when self atten (Source dim = Target dim)
-        
+        # online code does get same error rate as mine implementation (for non masked version)
+
         H = self.n_head
 
         q = self.query(query) # Q (N, S, E) XQ
@@ -178,7 +179,7 @@ class MultiHeadAttention(nn.Module):
         v_reshaped = torch.reshape(v, (N, T, H, E//H)).swapaxes(1,2) # (N, T, E) ->(N, T, H, E/H)->(N, H, T, E/H)
         
         qk_norm = torch.matmul(q_reshaped, k_reshaped.swapaxes(2,3))/torch.sqrt(torch.tensor(E/H)) #(N,H,S,E/H)*(N,H,E/H,T)->(N, H, S, T)
-        y = torch.matmul(self.attn_drop(F.softmax(qk_norm, dim = 2)), v_reshaped) # (N, H, S, T)(softmax along S slice)* (N,H,T,E/H) = (N,H,S,E/H)
+        y = torch.matmul(self.attn_drop(F.softmax(qk_norm, dim = 3)), v_reshaped) # (N, H, S, T)(softmax along S slice)* (N,H,T,E/H) = (N,H,S,E/H)
         output = self.proj(y.swapaxes(1, 2).reshape((N, S, E))) #(N, H, S, E/H) -> (N, S, H, E/H) -> (N,S,E)
         
 